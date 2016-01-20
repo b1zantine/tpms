c34e73221b21661eb64a380c6c1b85fdb4250d80
Sudar Abisheck <sudarabisheck@gmail.com>
2016-01-20 22:56:29 +0530

    Basic Grid Complete

--- app/src/main/java/me/sudar/tpms/MainActivity.java
+++ app/src/main/java/me/sudar/tpms/MainActivity.java
@@ -1,5 +1,6 @@
 package me.sudar.tpms;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -19,20 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        CustomGauge gauge1 = (CustomGauge) findViewById(R.id.gauge1);
-        CustomGauge gauge2 = (CustomGauge) findViewById(R.id.gauge2);
-        gauge1.setValue(800);
-        gauge2.setValue(500);
     }
 
     @Override
@@ -51,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(MainActivity.this,SettingsActivity.class));
             return true;
         }
 
