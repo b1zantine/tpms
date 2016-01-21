042fee43cc7433b864f5993e5e5556502cf87ec2
Prasanthi <prasanthi0409@gmail.com>
2016-01-21 10:28:08 +0530

    Revert "removed fragment comment"
    This reverts commit c07898a749cf8d10295aed7f176d387194ae93fd.

--- app/src/main/java/me/sudar/tpms/MainActivityFragment.java
+++ app/src/main/java/me/sudar/tpms/MainActivityFragment.java
@@ -6,7 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
+/**
+ * A placeholder fragment containing a simple view.
+ */
 public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
